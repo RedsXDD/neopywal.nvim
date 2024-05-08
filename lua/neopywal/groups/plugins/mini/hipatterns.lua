bfb853ddde732c8a9946a8c484ad97c7f27771ad
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-08 20:26:43 -0300

    Readded MiniHipatternNote highlight group.

--- lua/neopywal/groups/plugins/mini/hipatterns.lua
+++ lua/neopywal/groups/plugins/mini/hipatterns.lua
@@ -6,6 +6,7 @@ M.get = function(colors)
 		MiniHipatternsFixme = { bg = colors.color1, fg = colors.background, bold = true, italic = true },
 		MiniHipatternsHack  = { bg = colors.color11, fg = colors.background, bold = true, italic = true },
 		MiniHipatternsTodo  = { bg = colors.color4, fg = colors.background, bold = true, italic = true },
+		MiniHipatternsNote  = { bg = colors.foreground, fg = colors.background, bold = true, italic = true },
 	}
 end
 
