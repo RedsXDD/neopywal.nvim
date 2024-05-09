c5954b6a5cf5398718e588f6376799400de9dab6
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-09 15:32:22 -0300

    Started setting up configuration options.

--- lua/neopywal/init.lua
+++ lua/neopywal/init.lua
@@ -1,5 +1,84 @@
 local M = {}
 
+local defaults_options = {
+	fileformats = {
+		c_cpp = true,
+		clojure = true,
+		cmake = true,
+		commit = true,
+		c_sharp = true,
+		css = true,
+		dart = true,
+		diff = true,
+		elixir = true,
+		erlang = true,
+		go = true,
+		haskell = true,
+		help = true,
+		html = true,
+		ini = true,
+		java = true,
+		json = true,
+		js_react = true,
+		js = true,
+		kotlin = true,
+		latex = true,
+		less = true,
+		lisp = true,
+		lua = true,
+		makefile = true,
+		markdown = true,
+		matlab = true,
+		objectivec = true,
+		ocaml = true,
+		perl = true,
+		php = true,
+		power_shell = true,
+		python = true,
+		restructuredtext = true,
+		ruby = true,
+		rust = true,
+		sass = true,
+		scala = true,
+		shell = true,
+		swift = true,
+		toml = true,
+		typescript = true,
+		viml = true,
+		xml = true,
+		yaml = true,
+		zsh = true,
+	},
+	plugins = {
+		ale = true,
+		alpha = true,
+		bufferline = true,
+		cmp = true,
+		coc = true,
+		dashboard = true,
+		git_gutter = true,
+		indent_blankline = true,
+		lazy = true,
+		lspconfig = true,
+		neotree = true,
+		netrw = true,
+		telescope = true,
+		treesitter = true,
+		undotree = true,
+		which_key = true,
+		mini = {
+			cursorword = true,
+			files = true,
+			hipatterns = true,
+			indentscope = true,
+			pick = true,
+			starter = true,
+			statusline = true,
+			tabline = true,
+		},
+	}
+}
+
 function M.get_colors()
 	vim.cmd [[ source $HOME/.cache/wal/colors-wal.vim ]]
 
@@ -28,13 +107,16 @@ function M.get_colors()
 end
 
 local function apply_highlights(colors)
-	local base_highlights = require("neopywal.config").get_highlights(colors)
+	local config = require("neopywal.config")
+	local base_highlights = config.get_highlights(colors)
 	for group, properties in pairs(base_highlights) do
 		vim.api.nvim_set_hl(0, group, properties)
 	end
 end
 
-function M.setup()
+function M.setup(options)
+	M.options = vim.tbl_deep_extend("force", {}, defaults_options, options or {})
+
 	local colors = M.get_colors()
 	vim.opt.termguicolors = true
 	apply_highlights(colors)
