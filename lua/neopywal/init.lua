8b370b2832c974073dd95199c987c5193215a40e
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-10 12:57:30 -0300

    Replaced M.default_options with a local table declaration.

--- lua/neopywal/init.lua
+++ lua/neopywal/init.lua
@@ -1,87 +1,87 @@
-local M = {
-	default_options = {
-		default_fileformats = true,
-		fileformats = {
-			c_cpp = true,
-			clojure = true,
-			cmake = true,
-			git_commit = true,
-			c_sharp = true,
-			css = true,
-			dart = true,
-			diff = true,
-			elixir = true,
-			erlang = true,
-			go = true,
-			haskell = true,
-			help = true,
-			html = true,
-			ini = true,
-			java = true,
-			json = true,
-			javascript_react = true,
-			javascript = true,
-			kotlin = true,
-			latex = true,
-			less = true,
-			common_lisp = true,
-			lua = true,
-			makefile = true,
-			markdown = true,
-			matlab = true,
-			objectivec = true,
-			ocaml = true,
-			perl = true,
-			php = true,
-			powershell = true,
-			python = true,
-			restructuredtext = true,
-			ruby = true,
-			rust = true,
-			sass = true,
-			scala = true,
-			shell = true,
-			swift = true,
-			toml = true,
-			typescript = true,
-			viml = true,
-			xml = true,
-			yaml = true,
-			zsh = true,
-		},
-		default_plugins = true,
-		plugins = {
-			ale = true,
-			alpha = true,
-			bufferline = true,
-			nvim_cmp = true,
-			coc = true,
-			dashboard = true,
-			git_gutter = true,
-			indent_blankline = true,
-			lazy = true,
-			lspconfig = true,
-			neotree = true,
-			netrw = true,
-			telescope = true,
-			treesitter = true,
-			undotree = true,
-			which_key = true,
-			mini = {
-				cursorword = true,
-				files = true,
-				hipatterns = true,
-				indentscope = true,
-				pick = true,
-				starter = true,
-				statusline = true,
-				tabline = true,
-			},
+local M = {}
+
+local default_options = {
+	default_fileformats = true,
+	default_plugins = true,
+	fileformats = {
+		c_cpp = true,
+		clojure = true,
+		cmake = true,
+		git_commit = true,
+		c_sharp = true,
+		css = true,
+		dart = true,
+		diff = true,
+		elixir = true,
+		erlang = true,
+		go = true,
+		haskell = true,
+		help = true,
+		html = true,
+		ini = true,
+		java = true,
+		json = true,
+		javascript_react = true,
+		javascript = true,
+		kotlin = true,
+		latex = true,
+		less = true,
+		common_lisp = true,
+		lua = true,
+		makefile = true,
+		markdown = true,
+		matlab = true,
+		objectivec = true,
+		ocaml = true,
+		perl = true,
+		php = true,
+		powershell = true,
+		python = true,
+		restructuredtext = true,
+		ruby = true,
+		rust = true,
+		sass = true,
+		scala = true,
+		shell = true,
+		swift = true,
+		toml = true,
+		typescript = true,
+		viml = true,
+		xml = true,
+		yaml = true,
+		zsh = true,
+	},
+	plugins = {
+		ale = true,
+		alpha = true,
+		bufferline = true,
+		nvim_cmp = true,
+		coc = true,
+		dashboard = true,
+		git_gutter = true,
+		indent_blankline = true,
+		lazy = true,
+		lspconfig = true,
+		neotree = true,
+		netrw = true,
+		telescope = true,
+		treesitter = true,
+		undotree = true,
+		which_key = true,
+		mini = {
+			cursorword = true,
+			files = true,
+			hipatterns = true,
+			indentscope = true,
+			pick = true,
+			starter = true,
+			statusline = true,
+			tabline = true,
 		},
 	},
 }
 
-M.options = M.default_options
+M.options = default_options
 
 function M.get_colors()
 	vim.cmd([[ source $HOME/.cache/wal/colors-wal.vim ]])
@@ -116,7 +116,7 @@ local function apply_highlights(colors)
 		require("neopywal.theme.syntax").get(colors),
 		require("neopywal.theme.fileformats").get(colors),
 		require("neopywal.theme.plugins").get(colors),
-		{})
+	{})
 
 	for highlight_group, properties in pairs(theme) do
 		vim.api.nvim_set_hl(0, highlight_group, properties)
@@ -136,9 +136,9 @@ function M.setup(user_conf)
 
 	-- User configuration setup:
 	user_conf = user_conf or {}
-	if user_conf.default_fileformats == false then M.default_options.fileformats = {} end
-	if user_conf.default_plugins == false then M.default_options.plugins = {} end
-	M.options = vim.tbl_deep_extend("keep", {}, user_conf, M.default_options)
+	if user_conf.default_fileformats == false then default_options.fileformats = {} end
+	if user_conf.default_plugins == false then default_options.plugins = {} end
+	M.options = vim.tbl_deep_extend("keep", {}, user_conf, default_options)
 end
 
 return M
