e7fbb66683ab027bb39c04ffe43b240e38efe14d
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-11 20:41:22 -0300

    feat: Added support for custom user color variables.

--- lua/neopywal/init.lua
+++ lua/neopywal/init.lua
@@ -86,32 +86,34 @@ local default_options = {
 M.options = default_options
 
 function M.get_colors()
-	local user_colors = M.options.custom_colors
 	vim.cmd([[ source $HOME/.cache/wal/colors-wal.vim ]])
 
-	return {
-		none = "NONE",
-		transparent = "NONE",
-		background = user_colors.background or vim.g.background,
-		foreground = user_colors.foreground or vim.g.foreground,
-		cursor = user_colors.cursor or vim.g.cursor,
-		color0 = user_colors.color0 or vim.g.color0,
-		color1 = user_colors.color1 or vim.g.color1,
-		color2 = user_colors.color2 or vim.g.color2,
-		color3 = user_colors.color3 or vim.g.color3,
-		color4 = user_colors.color4 or vim.g.color4,
-		color5 = user_colors.color5 or vim.g.color5,
-		color6 = user_colors.color6 or vim.g.color6,
-		color7 = user_colors.color7 or vim.g.color7,
-		color8 = user_colors.color8 or vim.g.color8,
-		color9 = user_colors.color9 or vim.g.color9,
-		color10 = user_colors.color10 or vim.g.color10,
-		color11 = user_colors.color11 or vim.g.color11,
-		color12 = user_colors.color12 or vim.g.color12,
-		color13 = user_colors.color13 or vim.g.color13,
-		color14 = user_colors.color14 or vim.g.color14,
-		color15 = user_colors.color15 or vim.g.color15,
-	}
+	local user_colors = M.options.custom_colors
+
+	return vim.tbl_deep_extend("keep", {},
+		user_colors, {
+			none = "NONE",
+			transparent = "NONE",
+			background = vim.g.background,
+			foreground = vim.g.foreground,
+			cursor = vim.g.cursor,
+			color0 = vim.g.color0,
+			color1 = vim.g.color1,
+			color2 = vim.g.color2,
+			color3 = vim.g.color3,
+			color4 = vim.g.color4,
+			color5 = vim.g.color5,
+			color6 = vim.g.color6,
+			color7 = vim.g.color7,
+			color8 = vim.g.color8,
+			color9 = vim.g.color9,
+			color10 = vim.g.color10,
+			color11 = vim.g.color11,
+			color12 = vim.g.color12,
+			color13 = vim.g.color13,
+			color14 = vim.g.color14,
+			color15 = vim.g.color15,
+	}, {})
 end
 
 local did_setup = false
