901ad2ce54ac7774e421f27b5bdbdbac941e2740
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-11 23:25:37 -0300

    chore: Improved M.get_colors() code readability.

--- lua/neopywal/init.lua
+++ lua/neopywal/init.lua
@@ -89,31 +89,31 @@ function M.get_colors()
 	vim.cmd([[ source $HOME/.cache/wal/colors-wal.vim ]])
 
 	local user_colors = M.options.custom_colors
+	local pywal_colors = {
+		none = "NONE",
+		transparent = "NONE",
+		background = vim.g.background,
+		foreground = vim.g.foreground,
+		cursor = vim.g.cursor,
+		color0 = vim.g.color0,
+		color1 = vim.g.color1,
+		color2 = vim.g.color2,
+		color3 = vim.g.color3,
+		color4 = vim.g.color4,
+		color5 = vim.g.color5,
+		color6 = vim.g.color6,
+		color7 = vim.g.color7,
+		color8 = vim.g.color8,
+		color9 = vim.g.color9,
+		color10 = vim.g.color10,
+		color11 = vim.g.color11,
+		color12 = vim.g.color12,
+		color13 = vim.g.color13,
+		color14 = vim.g.color14,
+		color15 = vim.g.color15,
+	}
 
-	return vim.tbl_deep_extend("keep", {},
-		user_colors, {
-			none = "NONE",
-			transparent = "NONE",
-			background = vim.g.background,
-			foreground = vim.g.foreground,
-			cursor = vim.g.cursor,
-			color0 = vim.g.color0,
-			color1 = vim.g.color1,
-			color2 = vim.g.color2,
-			color3 = vim.g.color3,
-			color4 = vim.g.color4,
-			color5 = vim.g.color5,
-			color6 = vim.g.color6,
-			color7 = vim.g.color7,
-			color8 = vim.g.color8,
-			color9 = vim.g.color9,
-			color10 = vim.g.color10,
-			color11 = vim.g.color11,
-			color12 = vim.g.color12,
-			color13 = vim.g.color13,
-			color14 = vim.g.color14,
-			color15 = vim.g.color15,
-	}, {})
+	return vim.tbl_deep_extend("keep", {}, user_colors, pywal_colors)
 end
 
 local did_setup = false
