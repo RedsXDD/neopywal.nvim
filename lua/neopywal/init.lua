d918eda31f642ab9f569f49c232d75855d14475a
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-05-26 11:40:42 -0300

    refactor: moved colorizer.lua mix() function to init.lua

--- lua/neopywal/init.lua
+++ lua/neopywal/init.lua
@@ -84,8 +84,31 @@ local default_options = {
 		},
 	},
 }
-
 M.options = default_options
+
+function M.mix(color, option, factor)
+	-- Extract the RGB values from the color
+	local r, g, b = color:match("#(%x%x)(%x%x)(%x%x)")
+
+	-- Convert the RGB values from hexadecimal to decimal
+	r, g, b = tonumber(r, 16), tonumber(g, 16), tonumber(b, 16)
+
+	-- Apply the brightness factor to each RGB value
+	if option == "white" then
+		r = math.min(r + factor, 255)
+		g = math.min(g + factor, 255)
+		b = math.min(b + factor, 255)
+	elseif option == "black" then
+		r = math.min(r - factor, 255)
+		g = math.min(g - factor, 255)
+		b = math.min(b - factor, 255)
+	else
+		error("Could not parse color mixing options.")
+	end
+
+	-- Convert the RGB values back to hexadecimal and return the new color
+	return string.format("#%02x%02x%02x", r, g, b)
+end
 
 function M.get_colors()
 	if M.options.use_wallust then
@@ -124,22 +147,29 @@ end
 
 local did_setup = false
 function M.load()
-	if not did_setup then M.setup() end
+	if not did_setup then
+		M.setup()
+	end
 
 	local colors = M.get_colors()
 	vim.opt.termguicolors = true
 
 	local user_highlights = M.options.custom_highlights
-	if type(user_highlights) == "function" then user_highlights = user_highlights(colors) end
+	if type(user_highlights) == "function" then
+		user_highlights = user_highlights(colors)
+	end
 
 	require("neopywal.theme.terminal").get(colors)
-	local theme = vim.tbl_deep_extend("keep", {},
+	local theme = vim.tbl_deep_extend(
+		"keep",
+		{},
 		user_highlights,
 		require("neopywal.theme.ui").get(colors),
 		require("neopywal.theme.syntax").get(colors),
 		require("neopywal.theme.fileformats").get(colors),
 		require("neopywal.theme.plugins").get(colors),
-	{})
+		{}
+	)
 
 	for highlight_group, properties in pairs(theme) do
 		vim.api.nvim_set_hl(0, highlight_group, properties)
@@ -149,8 +179,12 @@ end
 function M.setup(user_conf)
 	did_setup = true
 	user_conf = user_conf or {}
-	if user_conf.default_fileformats == false then default_options.fileformats = {} end
-	if user_conf.default_plugins == false then default_options.plugins = {} end
+	if user_conf.default_fileformats == false then
+		default_options.fileformats = {}
+	end
+	if user_conf.default_plugins == false then
+		default_options.plugins = {}
+	end
 	M.options = vim.tbl_deep_extend("keep", {}, user_conf, default_options)
 end
 
