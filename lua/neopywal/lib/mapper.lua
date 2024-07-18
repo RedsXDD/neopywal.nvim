7fa0134a1e84fca9f719d0f942ea968b210165d0
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-18 14:38:23 -0300

    fix(mapper): boolean plugin option handling
    Fixed "mapper.lua" not properly handling boolean values for plugin
    options (specially for mini plugins).

--- lua/neopywal/lib/mapper.lua
+++ lua/neopywal/lib/mapper.lua
@@ -9,15 +9,6 @@ function M.get(theme_style)
 	C = require("neopywal").get_colors(theme_style)
 	U = require("neopywal.utils.color")
 
-	theme.editor = require("neopywal.theme.editor").get()
-
-	-- Get user-defined highlights.
-	local user_highlights = O.custom_highlights
-	if type(user_highlights) == "function" then
-		user_highlights = user_highlights(C)
-	end
-	theme.custom_highlights = user_highlights
-
 	-- Get highlights for enabled fileformats.
 	local fileformats = {}
 	for fileformat in pairs(O.fileformats) do
@@ -30,31 +21,53 @@ function M.get(theme_style)
 			fileformats = vim.tbl_deep_extend("force", fileformats, require("neopywal.theme.fileformats")[fileformat]())
 		end
 	end
-	theme.fileformats = fileformats
 
 	-- Get highlights for enabled plugins.
 	local plugins = {}
-	local function load_plugin(plugin, option)
-		if type(option) == "table" and option.enabled then
-			plugins = vim.tbl_deep_extend("force", plugins, require("neopywal.theme.plugins." .. plugin).get())
-		elseif option == true then
-			local default_config = require("neopywal").default_options.plugins[plugin]
-			O.plugins[plugin] = type(default_config) == "table" and default_config or {}
-			O.plugins[plugin].enabled = true
-			plugins = vim.tbl_deep_extend("force", plugins, require("neopywal.theme.plugins." .. plugin).get())
+	local function load_plugin(plugin, is_mini)
+		local option = is_mini and O.plugins.mini[plugin] or O.plugins[plugin]
+		local default_config = is_mini and require("neopywal").default_options.plugins.mini[plugin]
+			or require("neopywal").default_options.plugins[plugin]
+
+		local apply = false
+		if option == true or type(option) == "table" and option.enabled then
+			if is_mini == false then
+				O.plugins[plugin] = type(default_config) == "table" and default_config or {}
+				O.plugins[plugin].enabled = true
+			else
+				O.plugins.mini[plugin] = type(default_config) == "table" and default_config or {}
+				O.plugins.mini[plugin].enabled = true
+			end
+			apply = true
+		end
+
+		if apply then
+			local plugin_name = is_mini and "mini." .. plugin or plugin
+			plugins = vim.tbl_deep_extend("force", plugins, require("neopywal.theme.plugins." .. plugin_name).get())
 		end
 	end
 
-	for plugin, option in pairs(O.plugins) do
+	for plugin in pairs(O.plugins) do
 		if plugin == "mini" then
-			for mini_plugin, mini_option in pairs(option) do
-				load_plugin("mini." .. mini_plugin, mini_option)
+			for mini_plugin in pairs(O.plugins.mini) do
+				load_plugin(mini_plugin, true)
 			end
 		else
-			load_plugin(plugin, option)
+			load_plugin(plugin, false)
 		end
 	end
+
+	-- Get user-defined highlights.
+	local user_highlights = O.custom_highlights
+	if type(user_highlights) == "function" then
+		user_highlights = user_highlights(C)
+	end
+
+	-- This section MUST happen AFTER all the manipulation on the "O" table has been done.
+	theme.editor = require("neopywal.theme.editor").get()
+	theme.fileformats = fileformats
 	theme.plugins = plugins
+	theme.custom_highlights = user_highlights
 
 	O, C, U = _O, _C, _U -- Returning global variables
 
