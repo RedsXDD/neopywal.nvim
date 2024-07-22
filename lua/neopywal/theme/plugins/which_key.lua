2fadcbe160644d47c9231315feecaa3f87044b5a
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-22 12:17:32 -0300

    feat(which_key): improve main colors

--- lua/neopywal/theme/plugins/which_key.lua
+++ lua/neopywal/theme/plugins/which_key.lua
@@ -4,13 +4,13 @@ function M.get()
 	-- folke/which-key.nvim
 	return {
 		WhichKey = { fg = C.color1 },
-		WhichKeyGroup = { fg = C.color6 },
-		WhichKeyDesc = { fg = C.foreground },
-		WhichKeySeperator = { fg = U.blend(C.color1, C.color3, 0.5) },
-		WhichKeyValue = { fg = C.comment },
-		WhichKeySeparator = { fg = C.comment },
 		WhichKeyBorder = { link = "FloatBorder" },
-		WhichKeyFloat = { link = "NormalFloat" },
+		WhichKeyDesc = { fg = C.foreground },
+		WhichKeyFloat = { link = "WhichKeyNormal" },
+		WhichKeyGroup = { fg = C.color6 },
+		WhichKeyNormal = { bg = (O.transparent_background and vim.o.winblend == 0) and C.none or C.dim_bg },
+		WhichKeySeperator = { link = "WhichKeyValue" },
+		WhichKeyValue = { fg = C.comment },
 	}
 end
 
