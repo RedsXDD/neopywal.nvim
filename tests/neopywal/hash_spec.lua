64933887793da86490752515344aaa57bab45edd
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2024-07-28 12:34:17 -0300

    test(hash): fix improper hash tests

--- tests/neopywal/hash_spec.lua
+++ tests/neopywal/hash_spec.lua
@@ -1,19 +1,19 @@
 ---@diagnostic disable: undefined-global, undefined-field
 describe("hash", function()
     local hash = require("neopywal.lib.hashing").hash
-    it("typo", function() assert.are_not.equals(hash({ custom_highlight = {} }), hash({ ustom_highlight = {} })) end)
+    it("typo", function() assert.are_not.equals(hash({ custom_highlights = {} }), hash({ ustom_highlights = {} })) end)
     it(
         "when table order is shuffled",
         function()
             assert.equals(
                 hash({
-                    custom_highlight = {
+                    custom_highlights = {
                         Search = { fg = "#ff0000", bg = "#000000", style = { "bold" } },
                         IncSearch = { fg = "#000000", bg = "#ff0000" },
                     },
                 }),
                 hash({
-                    custom_highlight = {
+                    custom_highlights = {
                         Search = { style = { "bold" }, bg = "#000000", fg = "#ff0000" },
                         IncSearch = { bg = "#ff0000", fg = "#000000" },
                     },
@@ -25,27 +25,31 @@ describe("hash", function()
         "when toggle true/false",
         function()
             assert.are_not.equals({
-                fileformats = {
-                    c_cpp = true,
-                    c_sharp = true,
-                    clojure = true,
-                },
-                plugins = {
-                    alpha = true,
-                    noice = true,
-                    dashboard = true,
-                },
+                hash({
+                    fileformats = {
+                        c_cpp = true,
+                        c_sharp = true,
+                        clojure = true,
+                    },
+                    plugins = {
+                        alpha = true,
+                        noice = true,
+                        dashboard = true,
+                    },
+                }),
             }, {
-                fileformats = {
-                    c_cpp = true,
-                    c_sharp = false,
-                    clojure = false,
-                },
-                integrations = {
-                    alpha = true,
-                    noice = false,
-                    dashboard = false,
-                },
+                hash({
+                    fileformats = {
+                        c_cpp = true,
+                        c_sharp = false,
+                        clojure = false,
+                    },
+                    plugins = {
+                        alpha = true,
+                        noice = false,
+                        dashboard = false,
+                    },
+                }),
             })
         end
     )
