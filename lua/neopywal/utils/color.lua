7b5b6b112aec7c2c7efbe74cadf0424680ab0133
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2025-02-09 14:14:23 -0300

    feat(utils/color): support hsl color space
    Added support for HSL color manipulation with the new utils/hsluv.lua
    module.
    Also added some additional utility color functions on the
    utils/color.lua module to interact with the newly supported HSL color
    space.
    
    Functions added:
    - U.blacken(color, percentage)
    - U.brighten(color, percentage)
    - U.saturate(color, percentage)
    - U.invert(color)
    
    refactor(utils/color): rename "factor" on blend()
    
    Renamed the "factor" argument on the "blend()" color utility function to
    "percentage" so that it's more clear what input type the function can
    handle.

--- lua/neopywal/utils/color.lua
+++ lua/neopywal/utils/color.lua
@@ -1,5 +1,8 @@
 local M = {}
 
+local day_brightness = 0.3
+local hsluv = require("neopywal.utils.hsluv")
+
 local function ensureColor(color)
     local result = color
 
@@ -53,9 +56,33 @@ function M.hexToRgb(hex_color)
     }
 end
 
-function M.blend(color1, color2, factor)
+function M.RgbToHex(rgb)
+    local hexadecimal = "#"
+
+    for _, value in pairs(rgb) do
+        local hex = ""
+
+        while value > 0 do
+            local index = math.fmod(value, 16) + 1
+            value = math.floor(value / 16)
+            hex = string.sub(hsluv.hexChars, index, index) .. hex
+        end
+
+        if string.len(hex) == 0 then
+            hex = "00"
+        elseif string.len(hex) == 1 then
+            hex = "0" .. hex
+        end
+
+        hexadecimal = hexadecimal .. hex
+    end
+
+    return hexadecimal
+end
+
+function M.blend(color1, color2, percentage)
     -- Convert factor to a number if it's a hex string. Making sure it's betwheen 0 and 1.
-    factor = type(factor) == "string" and math.abs((tonumber(factor, 16) / 0xff)) or math.abs(factor)
+    percentage = type(percentage) == "string" and math.abs((tonumber(percentage, 16) / 0xff)) or math.abs(percentage)
 
     -- Convert hex colors to RGB.
     local rgb_color1 = M.hexToRgb(color1)
@@ -63,7 +90,7 @@ function M.blend(color1, color2, factor)
 
     -- Function to blend a single color channel.
     local blendChannel = function(i)
-        local result = (factor * rgb_color1[i] + ((1 - factor) * rgb_color2[i]))
+        local result = (percentage * rgb_color1[i] + ((1 - percentage) * rgb_color2[i]))
         return ensureColor(result)
     end
 
@@ -97,6 +124,69 @@ function M.lighten(color, factor)
 
     -- Convert the lightened RGB color back to hexadecimal and return it.
     return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
+end
+
+function M.blacken(color, percentage)
+    local hsl = hsluv.hex_to_hsluv(color)
+    local larpSpace = hsl[3]
+    if percentage < 0 then larpSpace = 1000 - hsl[3] end
+    hsl[3] = hsl[3] - larpSpace * percentage
+    return hsluv.hsluv_to_hex(hsl)
+end
+
+function M.brighten(color, percentage)
+    local hsl = hsluv.hex_to_hsluv(color)
+    local larpSpace = 100 - hsl[3]
+    if percentage < 0 then larpSpace = hsl[3] end
+    hsl[3] = hsl[3] + larpSpace * percentage
+    return hsluv.hsluv_to_hex(hsl)
+end
+
+function M.saturate(color, percentage)
+    local rgb = M.hexToRgb(color)
+
+    local saturation_float = percentage
+
+    table.sort(rgb)
+    local rgb_intensity = {
+        min = rgb[1] / 255,
+        mid = rgb[2] / 255,
+        max = rgb[3] / 255,
+    }
+
+    if rgb_intensity.max == rgb_intensity.min then
+        -- all colors have same intensity, which means
+        -- the original color is gray, so we can't change saturation.
+        return color
+    end
+
+    local new_intensities = {}
+    new_intensities.max = rgb_intensity.max
+    new_intensities.min = rgb_intensity.max * (1 - saturation_float)
+
+    if rgb_intensity.mid == rgb_intensity.min then
+        new_intensities.mid = new_intensities.min
+    else
+        local intensity_proportion = (rgb_intensity.max - rgb_intensity.mid) / (rgb_intensity.mid - rgb_intensity.min)
+        new_intensities.mid = (intensity_proportion * new_intensities.min + rgb_intensity.max)
+            / (intensity_proportion + 1)
+    end
+
+    for i, v in pairs(new_intensities) do
+        new_intensities[i] = math.floor(v * 255)
+    end
+    table.sort(new_intensities)
+    return (M.RgbToHex({ new_intensities.max, new_intensities.min, new_intensities.mid }))
+end
+
+function M.invert(color)
+    if color ~= "NONE" then
+        local hsl = hsluv.hex_to_hsluv(color)
+        hsl[3] = 100 - hsl[3]
+        if hsl[3] < 40 then hsl[3] = hsl[3] + (100 - hsl[3]) * day_brightness end
+        return hsluv.hsluv_to_hex(hsl)
+    end
+    return color
 end
 
 return M
