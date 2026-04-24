a5e2199d0792c01029ae722e9f441ff7bcae8067
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2026-04-24 01:49:25 -0300

    chore(palette): add dots to comments

--- lua/neopywal/lib/palette.lua
+++ lua/neopywal/lib/palette.lua
@@ -249,44 +249,44 @@ function M.get(theme_style, minimal_palette, extra_colors)
 
         -- Variable types:
         variable = C.color4, -- (preferred) any variable.
-        constant = C.color3, -- (preferred) any constant
-        string = C.foreground, -- a string constant: "this is a string"
-        character = C.color3, -- a character constant: 'c', '\n'
-        number = C.color5, -- a number constant: 234, 0xff
-        boolean = C.color5, -- a boolean constant: TRUE, FALSE
-        float = C.color5, -- a floating point constant: 2.3e10
-        identifier = U.blend(C.color1, C.color3, 0.5), -- (preferred) any variable name
-        func = C.color2, -- function name (also: methods for classes)
+        constant = C.color3, -- (preferred) any constant.
+        string = C.foreground, -- a string constant: "this is a string".
+        character = C.color3, -- a character constant: 'c', '\n'.
+        number = C.color5, -- a number constant: 234, 0xff.
+        boolean = C.color5, -- a boolean constant: TRUE, FALSE.
+        float = C.color5, -- a floating point constant: 2.3e10.
+        identifier = U.blend(C.color1, C.color3, 0.5), -- (preferred) any variable name.
+        func = C.color2, -- function name (also: methods for classes).
 
         -- Statements:
-        statement = C.color1, -- (preferred) any statement
+        statement = C.color1, -- (preferred) any statement.
         conditional = C.color1, -- if, then, else, endif, switch, etc.
         loop = C.color1, -- for, do, while, etc.
         label = C.color1, -- case, default, etc.
-        exception = C.color1, -- try, catch, throw
+        exception = C.color1, -- try, catch, throw.
         operator = C.color1, -- "sizeof", "+", "*", etc.
-        keyword = C.color1, -- any other keyword
+        keyword = C.color1, -- any other keyword.
         debug = C.color3, -- debugging statements.
 
         -- Preprocessors:
-        preproc = C.color5, -- (preferred) generic Preprocessor
-        include = C.color5, -- preprocessor #include
-        define = C.color5, -- preprocessor #define
-        macro = C.color5, -- same as Define
+        preproc = C.color5, -- (preferred) generic Preprocessor.
+        include = C.color5, -- preprocessor #include.
+        define = C.color5, -- preprocessor #define.
+        macro = C.color5, -- same as Define.
         precondit = C.color5, -- preprocessor #if, #else, #endif, etc.
 
         -- Type definitions:
         type = C.color6, -- (preferred) int, long, char, etc.
         structure = C.color6, -- struct, union, enum, etc.
         storageclass = C.color6, -- static, register, volatile, etc.
-        typedef = C.color6, -- A typedef
+        typedef = C.color6, -- A typedef.
 
         -- Special:
-        special = C.color5, -- (preferred) any special symbol
-        secialchar = C.color5, -- special character in a constant
-        tag = U.blend(C.color1, C.color3, 0.5), -- you can use CTRL-] on this
-        delimiter = C.foreground, -- character that needs attention
-        specialcomment = C.color8, -- special things inside a comment
+        special = C.color5, -- (preferred) any special symbol.
+        secialchar = C.color5, -- special character in a constant.
+        tag = U.blend(C.color1, C.color3, 0.5), -- you can use CTRL-] on this.
+        delimiter = C.foreground, -- character that needs attention.
+        specialcomment = C.color8, -- special things inside a comment.
     }
 
     ---@diagnostic disable-next-line: return-type-mismatch
