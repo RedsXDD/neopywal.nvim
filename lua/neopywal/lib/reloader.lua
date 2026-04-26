5fd3e5c369a332abe8d232490ff1358b5a6dde55
RedsXDD <161394370+RedsXDD@users.noreply.github.com>
2026-04-25 23:00:40 -0300

    fix(reloader): reload dark and light theme variants separately
    When developing the reloader module, i completely forgot that a user can
    set two distinct palettes for each of Neopywal's theme variants with the
    `use_palette` option on Neopywal's setup function. This caused an bug
    were the reloader was capable of detecting changes and reloading
    the colorscheme only for the `dark` theme variant. This has now been
    fixed.
    
    fix(reloader): debug augroup running on other colorschemes
    
    If you enable vim.g.neopywal_debug, Neopywal will automatically create a
    vim augroup for, well, debugging purposes. This commit addresses an
    issue
    were the augroup would still run even if you changed to completely
    different colorschemes.

--- lua/neopywal/lib/reloader.lua
+++ lua/neopywal/lib/reloader.lua
@@ -1,28 +1,50 @@
 local M = {}
 
-M.lock = false
-function M.init()
-    local Palette = require("neopywal.lib.palette")
-    if not Palette.did_setup then Palette.setup() end
-    if M.lock or Palette.palette_metadata.is_requireable then return end
-    M.lock = true
+local did_augroups = false
+---@param name string
+local function augroup(name) return vim.api.nvim_create_augroup("neopywal_" .. name, { clear = true }) end
 
-    if vim.g.neopywal_debug then
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = { "*/neopywal/*", "*/neopywal.lua" },
-            callback = function()
-                vim.schedule(function() require("neopywal.lib.compiler").recompile() end)
-            end,
-        })
+---@return function
+---@param schedule boolean
+---@param callback function
+local function setup_callback(schedule, callback)
+    local c = function()
+        if not vim.g.colors_name:match("^neopywal") then return end
+        callback()
     end
 
-    ---@diagnostic disable-next-line: undefined-field
-    local event
-    if vim.loop then event = vim.loop.new_fs_event() end -- luv binding in 0.9.
-    if vim.uv then event = vim.uv.new_fs_event() end -- luv binding from 0.10 onwards.
-    if event == nil then return end
+    local s = function()
+        if not vim.g.colors_name:match("^neopywal") then return end
+        vim.schedule(callback)
+    end
 
-    local template_path = Palette.options.use_palette[vim.o.background]
+    return schedule and s or c
+end
+
+local function augroups()
+    if not vim.g.neopywal_debug then return end
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        desc = "Source neopywal *.lua files for debug.",
+        group = augroup("debug_resource_files"),
+        pattern = { "*/neopywal/*", "*/neopywal.lua" },
+        callback = setup_callback(true, function() require("neopywal.lib.compiler").recompile() end),
+    })
+end
+
+M.lock = { dark = false, light = false }
+local function init(theme_style, template_path)
+    if not theme_style or theme_style ~= "dark" and theme_style ~= "light" then theme_style = vim.o.background end
+    if M.lock[theme_style] then return end
+    M.lock[theme_style] = true
+
+    vim.uv = vim.uv or vim.loop -- luv binding in 0.9 and 0.10 onwards.
+    local event = vim.uv.new_fs_event()
+    if event == nil then
+        local Notify = require("neopywal.utils.notify")
+        Notify.error("Failed to start Neopywal's reloader event ...")
+        return
+    end
+
     event:start(template_path, {
         watch_entry = true,
         stat = true,
@@ -40,8 +62,7 @@ You can open %s for debugging
 
 If you think this is a bug, kindly open an issue and attach %s file
 Below is the error message that we captured:
-%s
-        ]],
+%s]],
                 template_path,
                 err_path,
                 err_path,
@@ -58,22 +79,52 @@ Below is the error message that we captured:
 
         vim.schedule(function()
             if not vim.g.colors_name:match("^neopywal") then
-                M.lock = false
+                event:stop()
+                M.lock[theme_style] = false
                 package.loaded["neopywal.lib.reloader"] = nil
                 return
             end
 
-            local Notify = require("neopywal.utils.notify")
-            Notify.info(string.format(
-                [[
-Change detected in template file "%s",
-recompiling colorscheme ...
-]],
-                template_path
-            ))
+            local fetched_theme_style = theme_style
+            local current_theme_style = vim.o.background
+            if M.both == true or fetched_theme_style == current_theme_style then
+                local Notify = require("neopywal.utils.notify")
+                Notify.info(string.format(
+                    [[
+Change detected in template file
+"%s"
+for the "%s" theme style,
+recompiling colorscheme ...]],
+                    template_path,
+                    current_theme_style
+                ))
+            end
             require("neopywal.lib.compiler").recompile(false)
         end)
     end)
+end
+
+M.both = false
+function M.init()
+    if not did_augroups then augroups() end
+    local Palette = require("neopywal.lib.palette")
+    if not Palette.did_setup then Palette.setup() end
+
+    -- We really don't need two separate events for the same file.
+    if Palette.palette_metadata.dark.filepath == Palette.palette_metadata.light.filepath then
+        M.both = true
+        init("dark", Palette.palette_metadata.dark.filepath)
+        return
+    end
+
+    local template_path = ""
+    for _, theme_style in pairs({ "dark", "light" }) do
+        local metadata = Palette.palette_metadata[theme_style]
+        if metadata.file_exists and not metadata.is_requireable then
+            template_path = metadata.filepath
+            init(theme_style, template_path)
+        end
+    end
 end
 
 return M
